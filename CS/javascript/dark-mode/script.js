8bd42cf12d749dd55bf017c2fa8882afff9a63f8
NATHANPUGH22 <nathan.pugh@hgs.hiddengeniusproject.org>
2024-09-18 16:59:22 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,9 @@
 function darkMode() {
-// Your code here.
-}
+        if (document.body.style.backgroundColor === "white") {
+            document.body.style.backgroundColor = "navy";
+            document.body.style.color = "white";
+        } else {
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "navy";
+        }
+    }
