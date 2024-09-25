0702c85532ab6f973cd77f8e54af3d1e56eb08fe
NATHANPUGH22 <nathan.pugh@hgs.hiddengeniusproject.org>
2024-09-24 18:48:48 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,24 @@
-function modelPopUp() {
-    // Your code here.
+const modal = document.getElementById("myModal");
+
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
 }   
+
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+alertBtn.onclick =function() {
+    alert("This is a simple pop up alert think about how much you learned today. GO HIDDEN GENIUS PROJECT!!!");
+}
+
